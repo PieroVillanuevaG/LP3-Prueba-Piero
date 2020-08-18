7e1af4bf6a693ba8b11dc0ca3a57d76b0bb02499
Piero Villanueva <pierovillanueva.g@hotmail.com>
2020-08-17 21:29:23 -0500

    Metodo creado

--- metodo.py
+++ metodo.py
@@ -1,2 +1,10 @@
 # Autor: Piero Villanueva Gastello #
 # Ejercicio para ver si un alumno esta desaprobado o no#
+
+def verificar_aprobado(nota):
+    if(nota>=11):
+        resultado = "Aprobado";
+    else:
+        resultado = "Desaprobado";
+    return resultado
+
